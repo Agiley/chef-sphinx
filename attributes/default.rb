ce786e56f617a4d1e4499b42b8b101149f88db91
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2019-07-27 13:53:49 +0200

    Update sphinx 3 version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default[:sphinx][:version]          =   '3.0.2-2592786-linux-amd64'
+default[:sphinx][:version]          =   '3.1.1-612d99f-linux-amd64'
 default[:sphinx][:url]              =   "http://sphinxsearch.com/files/sphinx-#{node[:sphinx][:version]}.tar.gz"
 
 default[:sphinx][:install_path]     =   "/usr/local/bin"
